99fe5c5ba57eb30102680f144112521adb3ae88f
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-10-09 18:34:52 +0800

    main

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,8 +9,8 @@
             density="compact"
             nav
           >
-            <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-            <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+            <v-list-item prepend-icon="mdi-view-dashboard" title="Home" to="/" value="home"></v-list-item>
+            <v-list-item prepend-icon="mdi-forum" title="About" to="/manage" value="about"></v-list-item>
           </v-list>
         </v-navigation-drawer>
         <v-main style="height: 250px"></v-main>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <HelloWorld />
+  <Sidebar />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import Sidebar from '../components/Sidebar.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    Sidebar,
   },
+
 });
 </script>
