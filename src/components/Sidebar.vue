8b596095ee95a908152a0b29348f5074809ba4fe
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-10-09 19:03:03 +0800

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,55 @@
 <template>
-    <v-card>
-      <v-layout>
-        <v-navigation-drawer
-          floating
-          permanent
-        >
-          <v-list
-            density="compact"
-            nav
-          >
-            <v-list-item prepend-icon="mdi-view-dashboard" title="Home" to="/" value="home"></v-list-item>
-            <v-list-item prepend-icon="mdi-forum" title="About" to="/manage" value="about"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <v-main style="height: 250px"></v-main>
-      </v-layout>
-    </v-card>
-  </template>
+  <v-card
+    color="grey-lighten-4"
+    flat
+    height="200px"
+    rounded="0"
+  >
+    <v-toolbar density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
   
-  <script>
-  export default{
-      name: 'Sidebar',
-  }
-  </script>
+  <v-navigation-drawer app>
+    <v-list dense nav>
+      <v-list-item to="/" link>
+        <v-list-item-icon>
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/manage" link>
+        <v-list-item-icon>
+          <v-icon>mdi-forum</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+}
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
