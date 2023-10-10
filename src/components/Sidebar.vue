cf34e85b32345d77139fa80999d9f6fc8a8e0c4b
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-10-10 11:21:54 +0800

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,55 +1,86 @@
 <template>
-  <v-card
-    color="grey-lighten-4"
-    flat
-    height="200px"
-    rounded="0"
-  >
-    <v-toolbar density="compact">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-card>
+    <v-layout>
+      <!-- Header -->
+      <v-app-bar app dark color="primary">
+        <v-app-bar-nav-icon @click="toggleSidebar"></v-app-bar-nav-icon>
+        <v-toolbar-title>My App</v-toolbar-title>
+      </v-app-bar>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <!-- Sidebar -->
+      <v-navigation-drawer
+        v-model="sidebarOpen"
+        theme="dark"
+        rail
+        permanent
+      >
+        <v-list-item
+          nav
+          prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
+        ></v-list-item>
 
-      <v-spacer></v-spacer>
+        <v-divider></v-divider>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+        <v-list
+          density="compact"
+          nav
+        >
+          <v-list-item prepend-icon="mdi-view-dashboard" value="dashboard"></v-list-item>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+          <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
+      <!-- Main Content -->
+      <v-main style="height: 300px">
+        <div class="content">
+          <h1>Welcome to My App</h1>
+          <p>This is the main content of your sidebar.</p>
+          <p>You can add more content here.</p>
+        </div>
+      </v-main>
+
+      <!-- Footer -->
+      <v-footer app dark color="primary">
+        <span>&copy; 2023 My App</span>
+      </v-footer>
+    </v-layout>
   </v-card>
-  
-  <v-navigation-drawer app>
-    <v-list dense nav>
-      <v-list-item to="/" link>
-        <v-list-item-icon>
-          <v-icon>mdi-view-dashboard</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Home</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/manage" link>
-        <v-list-item-icon>
-          <v-icon>mdi-forum</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>About</v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
   name: 'Sidebar',
-}
+  data() {
+    return {
+      sidebarOpen: true, // You can control sidebar open/close with this variable
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    },
+  },
+};
 </script>
 
 <style scoped>
 /* Add your custom styles here */
+.content {
+  background-color: #f0f0f0;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+h1 {
+  font-size: 24px;
+  color: #333;
+}
+
+p {
+  font-size: 16px;
+  color: #555;
+}
 </style>
